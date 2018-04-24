6b9cece4b8d519e0ab1f0e0d14f2ede5f3e88c23
Olivier Poupeney <olivierpoupeney@MacBook-Pro-6.local>
2018-04-24 11:24:58 +0200

    Node templates are referring to npmjs package

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # generator-symphony
 Yeoman based generator for Symphony Bots and Applications
+
+## Installation
+
+``npm install -g generator-symphony``
+
+## Usage
+
+``yo symphony``
---
+++ generators/lib/certificate-creator/index.js
@@ -0,0 +1,69 @@
+const colors = require('colors');
+const certificate = require('selfsigned');
+const fs = require('fs');
+
+var CertificateCreator = {};
+
+CertificateCreator.attrs = [{
+    name: 'commonName',
+    value: 'to_be_replace_with_appname'
+}, {
+    name: 'countryName',
+    value: 'GB'
+}, {
+    shortName: 'ST',
+    value: 'GB'
+}, {
+    name: 'localityName',
+    value: 'London'
+}, {
+    name: 'organizationName',
+    value: 'Symphony Development Certificate'
+}, {
+    shortName: 'OU',
+    value: 'FOR TEST USE ONLY'
+}]
+
+CertificateCreator.create = (botusername, path) => {
+
+    /* Change the CName with the application Name */
+    var attrs = CertificateCreator.attrs.slice(0);
+    attrs[0].value = botusername;
+
+    certificate.generate( attrs, {
+        keySize: 2048, // the size for the private key in bits (default: 1024)
+        days: 365, // how long till expiry of the signed certificate (default: 365)
+        algorithm: 'sha256', // sign the certificate with specified algorithm (default: 'sha1')
+        extensions: [{
+            name: 'basicConstraints',
+            cA: true
+        }, {
+            name: 'keyUsage',
+            keyCertSign: true,
+            digitalSignature: true,
+            nonRepudiation: true,
+            keyEncipherment: true,
+            dataEncipherment: true
+        }], // certificate extensions array
+        pkcs7: true, // include PKCS#7 as part of the output (default: false)
+        clientCertificate: true, // generate client cert signed by the original key (default: false)
+        clientCertificateCN: botusername // client certificate's common name
+    }, function(err, pems) {
+        fs.writeFile(path + "/" + botusername + ".pem", pems.clientcert + "\n" +
+          pems.clientprivate + "\n" + pems.clientpublic + "\n" + pems.clientpkcs7 + "\n" +
+            pems.cert + "\n" + pems.fingerprint + "\n" + pems.private + "\n" + pems.public, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+        });
+        fs.writeFile(path + "/" + "ca-cer-" + botusername + ".cer", pems.cert, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+
+};
+
+module.exports = CertificateCreator;
