9242f8de238ceffee3e71bc73c1ee23382bded99
Reed Feldman <rfeldman@bates.edu>
2018-07-30 10:31:07 -0400

    python changes

--- generators/python-bots/index.js
+++ generators/python-bots/index.js
@@ -1,7 +1,6 @@
 const Generator = require('yeoman-generator');
 const colors = require('colors');
 const certificateCreator = require('../lib/p12-certificate-creator');
-const RSAcertificateCreator = require('../lib/certificate-creator');
 var mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
@@ -9,7 +8,7 @@ module.exports = class extends Generator {
     return this.prompt([
       {
         type    : 'list',
-        name    : 'java_bot_tpl',
+        name    : 'python_bot_tpl',
         message : 'Which template do you want to start with',
         choices : ['Request/Reply', 'NLP Based Trade Workflow']
       }
@@ -19,111 +18,85 @@ module.exports = class extends Generator {
       answers.dirname = this.options.initPrompts.dirname;
       answers.botusername = this.options.initPrompts.botusername;
       answers.botemail = this.options.initPrompts.botemail;
-      answers.encryption = this.options.initPrompts.encryption;
       let log_text = ('* Generating ' +
                      this.options.initPrompts.application_type.italic +
                      ' ' +
                      this.options.initPrompts.application_lang.italic +
                      ' code from ' +
-                     answers.java_bot_tpl.italic + ' template...').bold;
+                     answers.python_bot_tpl.italic + ' template...').bold;
       console.log(log_text.bgRed.white);
-
-      if (answers.encryption=='RSA') {
-        answers.authType = 'rsa';
-        answers.botCertPath = '';
-        answers.botCertName = '';
-        answers.botCertPassword = '';
-        answers.botRSAPath = answers.dirname + '/rsa/';
-        answers.botRSAName = 'rsa-private-' + answers.botusername + '.pem';
-      } else if (answers.encryption=='Self Signed Certificate') {
-        answers.authType = 'cert';
-        answers.botCertPath = answers.dirname + '/certificates/';
-        answers.botCertName = answers.botusername;
-        answers.botCertPassword = 'changeit';
-        answers.botRSAPath = '';
-        answers.botRSAName = '';
-      } else {
-        answers.authType = 'cert';
-        answers.botCertPath = '';
-        answers.botCertName = '';
-        answers.botCertPassword = '';
-        answers.botRSAPath = '';
-        answers.botRSAName = '';
-      }
-      if (answers.java_bot_tpl=='Request/Reply') {
+      if (answers.python_bot_tpl=='Request/Reply') {
         this.fs.copyTpl(
-          this.templatePath('java/bots/request-reply/pom.xml'),
-          this.destinationPath('pom.xml'),
+          this.templatePath('python/bots/request-reply/requirements.txt'),
+          this.destinationPath('requirements.txt'),
           answers
         );
         this.fs.copy(
-          this.templatePath('java/bots/request-reply/src'),
+          this.templatePath('python/bots/request-reply/src'),
           this.destinationPath('src')
         );
         this.fs.copyTpl(
-          this.templatePath('java/bots/request-reply/config.json'),
+          this.templatePath('python/bots/request-reply/config.json'),
           this.destinationPath('src/main/resources/config.json'),
           answers
         );
         this.fs.copy(
-          this.templatePath('java/bots/request-reply/certificates'),
+          this.templatePath('python/bots/request-reply/certificates'),
           this.destinationPath('certificates'),
           answers
         );
       }
-      if (answers.java_bot_tpl=='NLP Based Trade Workflow') {
+      if (answers.python_bot_tpl=='NLP Based Trade Workflow') {
         this.fs.copyTpl(
-          this.templatePath('java/bots/camunda-opennlp/pom.xml'),
-          this.destinationPath('pom.xml'),
+          this.templatePath('python/bots/camunda-opennlp/requirements.txt'),
+          this.destinationPath('requirements.txt'),
           answers
         );
         this.fs.copy(
-          this.templatePath('java/bots/camunda-opennlp/src'),
+          this.templatePath('python/bots/camunda-opennlp/src'),
           this.destinationPath('src')
         );
         this.fs.copyTpl(
-          this.templatePath('java/bots/request-reply/config.json'),
-          this.destinationPath('src/main/resources/config.json'),d
+          this.templatePath('python/bots/request-reply/config.json'),
+          this.destinationPath('src/main/resources/config.json'),
           answers
         );
         this.fs.copy(
-          this.templatePath('java/bots/camunda-opennlp/certificates'),
+          this.templatePath('python/bots/camunda-opennlp/certificates'),
           this.destinationPath('certificates')
         );
         this.fs.copyTpl(
-          this.templatePath('java/bots/camunda-opennlp/src/main/resources/nlp-config.json'),
+          this.templatePath('python/bots/camunda-opennlp/src/main/resources/nlp-config.json'),
           this.destinationPath('src/main/resources/nlp-config.json'),
           answers
         );
         this.fs.copy(
-          this.templatePath('java/bots/camunda-opennlp/bpmn'),
+          this.templatePath('python/bots/camunda-opennlp/bpmn'),
           this.destinationPath('bpmn')
         );
       }
       /* Install certificate */
-      console.log("generating from template "+ answers.java_bot_tpl);
       if (answers.encryption=='Self Signed Certificate') {
         let log_text_cert = ('* Generating certificate for BOT ' + answers.botusername + '...').bold;
         console.log(log_text_cert.bgRed.white);
+        mkdirp.sync( 'certificates' );
         certificateCreator.create( answers.botusername, 'certificates' );
       } else if (answers.encryption=='RSA') {
         let log_text_cert = ('* Generating RSA public/private keys for BOT ' + answers.botusername + '...').bold;
         console.log(log_text_cert.bgRed.white);
         mkdirp.sync( 'rsa' );
         RSAcertificateCreator.createRSA(answers.botusername, 'rsa' );
-        if (answers.java_bot_tpl=='Request/Reply') {
-          this.fs.copy(
-            this.templatePath('java/bots/request-reply/main-class-rsa/BotExample.java'),
-            this.destinationPath('src/main/java/BotExample.java')
-          );
-        } else {
-          this.fs.copy(
-          this.templatePath('java/bots/camunda-opennlp/main-class-rsa/BotExample.java'),
-          this.destinationPath('src/main/java/BotExample.java')
-        );
+        // if (answers.java_bot_tpl=='Request/Reply') {
+        //   this.fs.copy(
+        //     this.templatePath('java/bots/request-reply/main-class-rsa/BotExample.java'),
+        //     this.destinationPath('src/main/java/BotExample.java')
+        //   );
+        // } else {
+        //   this.fs.copy(
+        //   this.templatePath('java/bots/camunda-opennlp/main-class-rsa/BotExample.java'),
+        //   this.destinationPath('src/main/java/BotExample.java')
+        // );
         }
-
-      }
 
       let log_text_completion = ('* BOT generated successfully!!').bold;
       console.log(log_text_completion.bgGreen.white);
