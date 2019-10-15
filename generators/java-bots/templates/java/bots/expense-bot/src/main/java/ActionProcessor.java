4c3a91046175777dc4ec0034b176b4ba3c53b286
Vinay Mistry <vinay@symphony.com>
2019-10-15 11:24:08 +0100

    Feature/elements Java bot (#34)
    * feature/elements-nodejs-bot
    
    * feature/elements-java-bot

---
+++ generators/java-bots/templates/java/bots/expense-bot/src/main/java/ActionProcessor.java
@@ -0,0 +1,77 @@
+import clients.SymBotClient;
+import clients.symphony.api.UsersClient;
+import model.OutboundMessage;
+import model.UserInfo;
+import model.events.SymphonyElementsAction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ActionProcessor {
+    private SymBotClient botClient;
+
+    public ActionProcessor(SymBotClient botClient) {
+        this.botClient = botClient;
+    }
+
+    public void process(SymphonyElementsAction action) {
+        Map<String, Object> formValues =  action.getFormValues();
+
+        switch (action.getFormId()) {
+            case "add-expense-form": {
+                if (formValues.get("action").equals("add-expense-button")) {
+                    this.manageAddExpenseForm(action);
+                }
+                break;
+            }
+            case "expense-approval-form": {
+                if (formValues.get("action").equals("submit-expense")) {
+                    this.manageExpenseApprovalForm(action);
+                }
+                break;
+            }
+        }
+    }
+
+    public void manageAddExpenseForm(SymphonyElementsAction action) {
+        Map<String, Object> formValues =  action.getFormValues();
+        String vendor = (String)formValues.get("vendor-textfield");
+        String date = (String)formValues.get("date-textfield");
+        String price = (String)formValues.get("price-textfield");
+
+        Expense newExpense = new Expense(vendor, date, price);
+        ExpenseManager.getInstance().addExpense(newExpense);
+
+        OutboundMessage messageOut = MessageSender.getInstance().buildCreateFormMessage();
+
+        MessageSender.getInstance().sendMessage(action.getStreamId(), messageOut);
+    }
+
+    public void manageExpenseApprovalForm(SymphonyElementsAction action) {
+        Map<String, Object> formValues =  action.getFormValues();
+        ArrayList<Long> referals = (ArrayList<Long>)formValues.get("person-selector");
+
+        if (referals.size() > 0) {
+            UsersClient usersClient = this.botClient.getUsersClient();
+
+            try {
+                List<UserInfo> usersInfo = usersClient.getUsersFromIdList((List<Long>)referals, false);
+                String referalUsers = usersInfo.stream().map(UserInfo::getDisplayName).collect(Collectors.joining(","));
+
+                OutboundMessage messageOut = MessageSender.getInstance().buildConfirmMessage(referalUsers);
+
+                MessageSender.getInstance().sendMessage(action.getStreamId(), messageOut);
+
+                ExpenseManager.getInstance().reset();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            OutboundMessage messageOut = MessageSender.getInstance().buildCreateFormMessage(true);
+
+            MessageSender.getInstance().sendMessage(action.getStreamId(), messageOut);
+        }
+    }
+}
