65d4f00830dbedc005c09d3caa1757bc01c67b5d
Elias Croze <elias.croze@symphony.com>
2020-06-08 09:51:59 +0200

    Auto-indented files

--- generators/dotnet-bots/index.js
+++ generators/dotnet-bots/index.js
@@ -1,16 +1,14 @@
 const Generator = require('yeoman-generator');
-const colors = require('colors');
 const certificateCreator = require('../lib/p12-certificate-creator');
-var mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
   prompting() {
     return this.prompt([
       {
-        type    : 'list',
-        name    : 'dotnet_bot_tpl',
-        message : 'Which template do you want to start with',
-        choices : ['Request/Reply', 'NLP Based Trade Order', 'Indication Of Interest', 'Trade Alert']
+        type: 'list',
+        name: 'dotnet_bot_tpl',
+        message: 'Which template do you want to start with',
+        choices: ['Request/Reply', 'NLP Based Trade Order', 'Indication Of Interest', 'Trade Alert']
       }
     ]).then((answers) => {
       answers.application_name = this.options.initPrompts.application_name;
@@ -22,13 +20,13 @@ module.exports = class extends Generator {
       answers.botemail = this.options.initPrompts.botemail;
 
       let log_text = ('* Generating ' +
-                     this.options.initPrompts.application_type.italic +
-                     ' ' +
-                     this.options.initPrompts.application_lang.italic +
-                     ' code from ' +
-                     answers.dotnet_bot_tpl.italic + ' template...').bold;
+        this.options.initPrompts.application_type.italic +
+        ' ' +
+        this.options.initPrompts.application_lang.italic +
+        ' code from ' +
+        answers.dotnet_bot_tpl.italic + ' template...').bold;
       console.log(log_text.bgRed.white);
-      if (answers.dotnet_bot_tpl=='Request/Reply') {
+      if (answers.dotnet_bot_tpl == 'Request/Reply') {
         this.fs.copy(
           this.templatePath('dotnet/bots/request-reply/RequestResponse.csproj'),
           this.destinationPath('RequestResponse.csproj')
@@ -52,11 +50,11 @@ module.exports = class extends Generator {
           answers
         );
         /* Install certificate */
-      if (this.options.initPrompts.selfsigned_certificate=='Yes') {
-        let log_text_cert = ('* Generating certificate for BOT ' + this.options.initPrompts.botusername + '...').bold;
-        console.log(log_text_cert.bgRed.white);
-        certificateCreator.create( this.options.initPrompts.botusername, this.options.initPrompts.botemail );
-      }
+        if (this.options.initPrompts.selfsigned_certificate == 'Yes') {
+          let log_text_cert = ('* Generating certificate for BOT ' + this.options.initPrompts.botusername + '...').bold;
+          console.log(log_text_cert.bgRed.white);
+          certificateCreator.create(this.options.initPrompts.botusername, this.options.initPrompts.botemail);
+        }
       }
     });
   }
--- generators/lib/p12-certificate-creator/index.js
+++ generators/lib/p12-certificate-creator/index.js
@@ -1,10 +1,10 @@
 const p12 = require('node-openssl-p12');
 const fs = require('fs');
-var mkdirp = require('mkdirp');
+const mkdirp = require('mkdirp');
 const selfsigned = require('selfsigned');
 
 
-var P12CertificateCreator = {};
+let P12CertificateCreator = {};
 
 P12CertificateCreator.attrs = [{
     name: 'commonName',
@@ -29,9 +29,8 @@ P12CertificateCreator.attrs = [{
 P12CertificateCreator.create = (botusername, botemail) => {
     var attrs = P12CertificateCreator.attrs.slice(0);
     attrs[0].value = botusername;
-    
-    
-    selfsigned.generate( attrs, {
+
+    selfsigned.generate(attrs, {
         keySize: 2048, // the size for the private key in bits (default: 1024)
         days: 365, // how long till expiry of the signed certificate (default: 365)
         algorithm: 'sha256', // sign the certificate with specified algorithm (default: 'sha1')
@@ -45,25 +44,26 @@ P12CertificateCreator.create = (botusername, botemail) => {
             nonRepudiation: true,
             keyEncipherment: true,
             dataEncipherment: true
-        }], 
-    }, function(err, pems) {
-        mkdirp.sync( 'ssl' );
-        
-        fs.writeFile('ssl/'+botusername+'-ca.key', pems.private , function(err) {
+        }],
+    }, function (err, pems) {
+        mkdirp.sync('ssl');
+
+        fs.writeFile('ssl/' + botusername + '-ca.key', pems.private, function (err) {
             if (err) {
                 return console.log(err);
             }
 
         });
-        fs.writeFile('ssl/'+botusername+'-ca.crt', pems.cert, function(err) {
-          if (err) {
-              return console.log(err);
-          }});
+        fs.writeFile('ssl/' + botusername + '-ca.crt', pems.cert, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
 
-          var p12options = {
+        var p12options = {
             bitSize: 2048,
-            clientFileName : botusername,
-            C:'EX',
+            clientFileName: botusername,
+            C: 'EX',
             ST: 'Example',
             L: 'Example',
             O: 'Example',
@@ -71,33 +71,27 @@ P12CertificateCreator.create = (botusername, botemail) => {
             CN: botusername,
             emailAddress: botemail,
             clientPass: 'changeit',
-            caFileName: botusername+'-ca',
+            caFileName: botusername + '-ca',
             serial: '01',
             days: 365
         };
-        p12.createClientSSL(p12options).done(function(options, sha1fingerprint) {
-            fs.unlink('ssl/'+botusername+'-ca.key', function (err) {
-                        if (err) throw err;
-            });
-            fs.rename('ssl/'+botusername+'-ca.crt', 'certificates/'+botusername+'-ca.crt', function (err) {
-            if (err) throw err;
-            });
-            fs.rename('ssl/'+botusername+'.p12', 'certificates/'+botusername+'.p12', function (err) {
+        p12.createClientSSL(p12options).done(function (options, sha1fingerprint) {
+            fs.unlink('ssl/' + botusername + '-ca.key', function (err) {
                 if (err) throw err;
-                });
+            });
+            fs.rename('ssl/' + botusername + '-ca.crt', 'certificates/' + botusername + '-ca.crt', function (err) {
+                if (err) throw err;
+            });
+            fs.rename('ssl/' + botusername + '.p12', 'certificates/' + botusername + '.p12', function (err) {
+                if (err) throw err;
+            });
             fs.rmdir("ssl", function (err) {
                 if (err) throw err;
             });
-        }).fail( function(err) {
+        }).fail(function (err) {
             console.log(err);
         });
-       
-
     });
-
-
-
-    
 }
 
 module.exports = P12CertificateCreator;
