a2e56896d162bbb4d5f4093eb6c3563b97870c26
symphony-thibault <thibault.pensec@symphony.com>
2020-09-10 18:19:47 +0200

    APP-3037 Update Yeoman generator for BDK2.0

---
+++ generators/2.0/index.js
@@ -0,0 +1,52 @@
+const Generator = require('yeoman-generator');
+const colors = require('colors');
+const packageJson = require('../../package.json');
+
+module.exports = class extends Generator {
+
+    constructor (args, opts) {
+        super(args, opts);
+    }
+
+    initializing () {
+
+        this.log(` __   __     ___                 _                 
+ \\ \\ / /__  / __|_  _ _ __  _ __| |_  ___ _ _ _  _ 
+  \\ V / _ \\ \\__ \\ || | '  \\| '_ \\ ' \\/ _ \\ ' \\ || |
+   |_|\\___/ |___/\\_, |_|_|_| .__/_||_\\___/_||_\\_, |
+                 |__/      |_|                |__/ `.blue);
+        this.log('\thttps://developers.symphony.com\n');
+        this.log('Welcome to Symphony Generator '.gray + `v${packageJson.version}`.yellow);
+        this.log('Application files will be generated in folder: '.gray + `${this.destinationRoot()}`.yellow);
+        this.log('______________________________________________________________________________________________________'.yellow);
+    }
+
+    async prompting () {
+        this.answers = await this.prompt([
+            {
+                type: 'input',
+                name: 'host',
+                message: 'Enter your pod host',
+                default: 'acme.symphony.com'
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'Enter your bot username',
+                default: 'my-bot'
+            },
+            {
+                type: 'list',
+                name: 'language',
+                message: 'Select your programing language',
+                choices: [
+                    'Java (beta)'
+                ]
+            }
+        ]);
+
+        if (this.answers.language === 'Java (beta)') {
+            this.composeWith(require.resolve('./java'), this.answers);
+        }
+    }
+}
