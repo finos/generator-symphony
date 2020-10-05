3229585d144442a3cadd8c71f7dfe7bddd60c521
symphony-hong <hong.le@symphony.com>
2020-10-05 12:56:50 +0200

    Update Thibault's comments

--- generators/bdk/java/index.js
+++ generators/bdk/java/index.js
@@ -40,7 +40,7 @@ module.exports = class extends Generator {
                         value: 'java'
                     },
                     {
-                        name: 'SpringBoot (experimental)',
+                        name: 'Spring Boot (experimental)',
                         value: 'spring'
                     }
                 ]
