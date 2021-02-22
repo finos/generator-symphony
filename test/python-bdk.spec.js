92ce69ffec1c124f905e19db7fac23203ddbb73d
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2021-02-22 05:15:15 -0800

    PLAT-10418: Python BDK generator (#99)
    * SpringBoot bot generator
    
    * Python BDK 2.0 bot project generator
    
    Add template of a bot project using Python BDK 2.0b0
    
    index.js for an entry to generate the Python bot project
    
    Added unittest
    
    * Improve the log and unittest

---
+++ test/python-bdk.spec.js
@@ -0,0 +1,58 @@
+const helpers = require('yeoman-test')
+const assert = require('yeoman-assert')
+const path = require('path')
+const fs = require('fs')
+const forge = require('node-forge')
+
+const BASE_RESOURCE = 'resources'
+const BASE_PYTHON = 'python'
+const SMALL_KEY_PAIR_LENGTH = 512;
+
+describe('Python BDK', () => {
+  const currentDir = process.cwd()
+
+  afterAll(() => process.chdir(currentDir))
+
+  it('Generate 2.0 python bot', () => {
+    return helpers.run(path.join(__dirname, '../generators/bdk'))
+      .inTmpDir()
+      .withLocalConfig({
+        KEY_PAIR_LENGTH: SMALL_KEY_PAIR_LENGTH
+      })
+      .withPrompts({
+        host: 'acme.symphony.com',
+        username: 'test-bot',
+        application: 'bot-app',
+        language: 'python',
+        appId: 'app-id'
+      }).then((dir) => {
+        assert.file([
+          path.join(BASE_PYTHON, 'main.py'),
+          path.join(BASE_RESOURCE, 'config.yaml'),
+          'rsa/privatekey.pem',
+          'rsa/publickey.pem',
+          'requirements.txt',
+          'logging.conf',
+          '.gitignore'
+        ]);
+        let privateKey = fs.readFileSync(path.join(dir, 'rsa/privatekey.pem'), 'utf-8')
+        let generatedPublicKey = fs.readFileSync(path.join(dir, 'rsa/publickey.pem'), 'utf-8')
+        assertKeyPair(privateKey, generatedPublicKey)
+
+        let requirement = fs.readFileSync(path.join(dir, 'requirements.txt'), 'utf-8')
+        let config = fs.readFileSync(path.join(dir, BASE_RESOURCE, 'config.yaml'), 'utf-8')
+        let version_regex = /sym-api-client-python>=\d.\d+(\w\d+)?/i
+        assert(requirement.match(version_regex))
+        assert(config.includes('host: acme.symphony.com'))
+        assert(config.includes('username: test-bot'))
+        assert(config.includes('path: rsa/privatekey.pem'))
+      })
+  })
+})
+
+function assertKeyPair(generatedPrivateKey, generatedPublicKey) {
+  let forgePrivateKey = forge.pki.privateKeyFromPem(generatedPrivateKey);
+  let forgePublicKey = forge.pki.setRsaPublicKey(forgePrivateKey.n, forgePrivateKey.e);
+  let publicKey = forge.pki.publicKeyToRSAPublicKeyPem(forgePublicKey).toString();
+  assert.textEqual(generatedPublicKey.split("\n").join(""), publicKey.split("\n").join(""))
+}
