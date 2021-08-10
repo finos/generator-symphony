ca8813e97a9ce05b2d878368898f53de0f723ce5
Soufiane Aourinmouche <52406574+symphony-soufiane@users.noreply.github.com>
2021-08-10 16:46:51 +0200

    PLAT-11223: release 1.7.6 (#131)
    Backported:
     - (#127)
     - (#129)
    
    Bumped the version to prepare release

--- generators/bdk/java/index.js
+++ generators/bdk/java/index.js
@@ -7,8 +7,8 @@ const COMMON_EXT_APP_TEMPLATES = '../../../common-template/circle-of-trust-ext-a
 const BASE_JAVA = 'src/main/java';
 const BASE_RESOURCES = 'src/main/resources';
 
-const BDK_VERSION_DEFAULT = '2.1.1';
-const SPRING_VERSION_DEFAULT = '2.4.1'
+const BDK_VERSION_DEFAULT = '2.2.1';
+const SPRING_VERSION_DEFAULT = '2.5.3'
 
 // Make it configurable for faster test execution
 const KEY_PAIR_LENGTH = 'KEY_PAIR_LENGTH';
