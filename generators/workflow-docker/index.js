f9b64555c76b655260e2e2fb020535954222e737
Yong Sheng Tan <yong.tan@symphony.com>
2023-06-13 14:22:09 +0200

    Skip key generation for sandbox pod in WDK Docker projects

--- generators/workflow-docker/index.js
+++ generators/workflow-docker/index.js
@@ -11,31 +11,32 @@ module.exports = class extends Generator {
   async writing() {
 
     mkdirp.sync('symphony/workflows');
-    mkdirp.sync('symphony/libs');
 
-    try {
-      this.log('Generating RSA keys...'.green.bold);
-      this.pair = keyPair(this.config.get(KEY_PAIR_LENGTH) || 4096);
-      this.fs.write(this.destinationPath('symphony/publickey.pem'), this.pair.public, err => this.log.error(err));
-      this.fs.write(this.destinationPath('symphony/privatekey.pem'), this.pair.private, err => this.log.error(err));
-      this.options.privateKeyPath = 'symphony/privatekey.pem';
-      this.options.randomKey = crypto.randomBytes(16).toString('hex');
-    } catch (e) {
-      this.log.error(`Oups, something went wrong when generating RSA key pair`, e);
+    if (this.options.host !== 'develop2.symphony.com') {
+      try {
+        this.log('Generating RSA keys...'.green.bold);
+        this.pair = keyPair(this.config.get(KEY_PAIR_LENGTH) || 4096);
+        this.fs.write(this.destinationPath('symphony/publickey.pem'), this.pair.public, err => this.log.error(err));
+        this.fs.write(this.destinationPath('symphony/privatekey.pem'), this.pair.private, err => this.log.error(err));
+      } catch (e) {
+        this.log.error(`Oups, something went wrong when generating RSA key pair`, e);
+      }
     }
+    this.options.randomKey = crypto.randomBytes(16).toString('hex');
 
     this.fs.copyTpl(
-      this.templatePath(`application.yaml.ejs`),
-      this.destinationPath(`symphony/application.yaml`),
+      this.templatePath('application.yaml.ejs'),
+      this.destinationPath('symphony/application.yaml'),
       this.options
     )
-
-    this.fs.copyTpl(
-      this.templatePath(`startup.sh.ejs`),
-      this.destinationPath(`startup.sh`),
-      this.options
+    this.fs.copy(
+      this.templatePath('startup.sh'),
+      this.destinationPath('startup.sh')
     )
-
+    this.fs.copy(
+      this.templatePath('hello.swadl.yaml'),
+      this.destinationPath('symphony/workflows/hello.swadl.yaml')
+    )
   }
 
   end() {
@@ -45,6 +46,8 @@ module.exports = class extends Generator {
         ` with the following public key on https://${this.options.host}/admin-console : `.cyan);
 
       this.log('\n' + this.pair.public);
+    } else {
+      this.log('\nYou can now place the private key you received in the '.yellow + 'rsa'.white + ' directory\n'.yellow);
     }
 
     this.log(`Your workflow bot has been successfully generated !`.cyan.bold);
