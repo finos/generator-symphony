ac65d1c8d6c2a7b85c18bfa09acc0fff2c23982f
Yong Sheng Tan <yong.tan@symphony.com>
2023-07-11 18:39:18 +0800

    Revised WDK project generation

--- generators/java/index.js
+++ generators/java/index.js
@@ -1,22 +1,22 @@
-const Generator = require('yeoman-generator');
-const path = require('path');
-const fs = require('fs');
-const axios = require('axios');
-const keyPair = require('../_lib/rsa').keyPair;
+const Generator = require('yeoman-generator')
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+const keyPair = require('../_lib/rsa').keyPair
 
 const COMMON_EXT_APP_TEMPLATES = '../../_common/circle-of-trust-ext-app'
-const BASE_JAVA = 'src/main/java';
-const BASE_RESOURCES = 'src/main/resources';
+const BASE_JAVA = 'src/main/java'
+const BASE_RESOURCES = 'src/main/resources'
 
-const BDK_VERSION_DEFAULT = '2.14.1';
+const BDK_VERSION_DEFAULT = '2.14.1'
 const SPRING_VERSION_DEFAULT = '2.7.12'
 
 // Make it configurable for faster test execution
-const KEY_PAIR_LENGTH = 'KEY_PAIR_LENGTH';
+const KEY_PAIR_LENGTH = 'KEY_PAIR_LENGTH'
 
 const _getVersion = () => {
-  return axios.get('https://search.maven.org/solrsearch/select?q=g:org.finos.symphony.bdk')
-    .then(res => res.data);
+  const uri = 'https://search.maven.org/solrsearch/select?q=g:org.finos.symphony.bdk+AND+a:symphony-bdk-bom&core=gav'
+  return axios(uri, { timeout: 5000 }).then(res => res.data)
 }
 
 module.exports = class extends Generator {
@@ -33,12 +33,6 @@ module.exports = class extends Generator {
       },
       {
         type: 'input',
-        name: 'groupId',
-        message: 'Enter your project groupId',
-        default: 'com.mycompany'
-      },
-      {
-        type: 'input',
         name: 'artifactId',
         message: 'Enter your project artifactId',
         default: 'bot-application'
@@ -49,128 +43,96 @@ module.exports = class extends Generator {
         message: 'Enter your base package',
         default: 'com.mycompany.bot'
       }
-    ]);
+    ])
   }
 
   async writing() {
-    let basePackage = this.answers.basePackage.split('.').join('/');
+    this.log()
+    let basePackage = this.answers.basePackage.replaceAll(/\./g, '/')
 
     // copy input options as answers to be used in templates
-    this.answers.host = this.options.host;
-    this.answers.username = this.options.username;
-    this.answers.framework = this.options.framework;
-    this.answers.application = this.options.application;
-    this.answers.appId = this.options.appId;
-
-    this.answers.bdkBomVersion = BDK_VERSION_DEFAULT;
-    this.answers.springBootVersion = SPRING_VERSION_DEFAULT;
+    this.answers = {
+      framework: 'java',
+      ...this.options,
+      ...this.answers,
+      bdkBomVersion: BDK_VERSION_DEFAULT,
+      springBootVersion: SPRING_VERSION_DEFAULT,
+    }
 
     await _getVersion().then(response => {
       if (response['response']['docs'].length === 0) {
-        console.log(`Failed to fetch latest Java BDK version from Maven Central, ${this.answers.bdkBomVersion} will be used.`);
+        console.log(`Failed to fetch latest Java BDK version from Maven Central, ${this.answers.bdkBomVersion} will be used.`)
       } else {
-        this.answers.bdkBomVersion = response['response']['docs'][0]['latestVersion'];
+        this.answers.bdkBomVersion = response['response']['docs'].filter(d => !d.id.match(/(RC|alpha)/g))[0].v
       }
     }).catch(err => {
-      console.log(`Failed to fetch latest Java BDK version from Maven Central, ${this.answers.bdkBomVersion} will be used.`);
-      console.log(`The request failed because of: {errno: ${err.errno}, code: ${err.code}}`);
-    });
+      console.log(`Failed to fetch latest Java BDK version from Maven Central, ${this.answers.bdkBomVersion} will be used.`)
+      console.log(`The request failed because of: {errno: ${err.errno}, code: ${err.code}}`)
+    })
+    this.log(`BDK Version: ${this.answers.bdkBomVersion}`)
 
     if (this.answers.host !== 'develop2.symphony.com') {
       try {
-        this.log('Generating RSA keys...'.green.bold);
-        this.pair = keyPair(this.config.get(KEY_PAIR_LENGTH) || 4096);
-        this.fs.write(this.destinationPath('rsa/publickey.pem'), this.pair.public, err => this.log.error(err));
-        this.fs.write(this.destinationPath('rsa/privatekey.pem'), this.pair.private, err => this.log.error(err));
+        this.log('Generating RSA keys...'.green)
+        this.pair = keyPair(this.config.get(KEY_PAIR_LENGTH) || 4096)
+        this.fs.write(this.destinationPath('rsa/publickey.pem'), this.pair.public, err => this.log.error(err))
+        this.fs.write(this.destinationPath('rsa/privatekey.pem'), this.pair.private, err => this.log.error(err))
       } catch (e) {
-        this.log.error(`Oups, something went wrong when generating RSA key pair`, e);
+        this.log.error(`Oups, something went wrong when generating RSA key pair`, e)
       }
     }
-    this.answers.privateKeyPath = 'rsa/privatekey.pem';
+    this.answers.privateKeyPath = 'rsa/privatekey.pem'
 
     if (this.answers.application === 'bot-app') { // Bot application
       // check if framework is setup or not
-      switch (this.answers.framework) {
-        case 'java':
-          // process and copy config.yaml file
-          this.fs.copyTpl(
-            this.templatePath(path.join(this.answers.framework, 'config.yaml.ejs')),
-            this.destinationPath(path.join(BASE_RESOURCES, 'config.yaml')),
-            this.answers
-          );
-          break;
-        case 'spring':
-          // process and copy application.yaml.ejs file
-          this.fs.copyTpl(
-            this.templatePath(path.join(this.answers.framework, 'application.yaml.ejs')),
-            this.destinationPath(path.join(BASE_RESOURCES, 'application.yaml')),
-            this.answers
-          );
-          break;
+      if (this.answers.framework === 'java') {
+        // process and copy config.yaml file
+        this._copyTpl(
+          path.join(this.answers.framework, 'config.yaml'),
+          path.join(BASE_RESOURCES, 'config.yaml')
+        )
+      } else {
+        // process and copy application.yaml.ejs file
+        this._copyTpl(
+          path.join(this.answers.framework, 'application.yaml'),
+          path.join(BASE_RESOURCES, 'application.yaml')
+        )
       }
 
       // process and copy template file
-      this.fs.copyTpl(
-        this.templatePath('gif.ftl'),
-        this.destinationPath(path.join(BASE_RESOURCES, 'templates', 'gif.ftl'))
-      );
-      this.fs.copyTpl(
-        this.templatePath('welcome.ftl'),
-        this.destinationPath(path.join(BASE_RESOURCES, 'templates', 'welcome.ftl'))
-      );
+      this._copy('gif.ftl', path.join(BASE_RESOURCES, 'templates', 'gif.ftl'))
+      this._copy('welcome.ftl', path.join(BASE_RESOURCES, 'templates', 'welcome.ftl'))
 
       // Process Framework specific files
-      this._copyJavaTemplate(this.answers.framework, basePackage);
+      this._copyJavaTemplate(this.answers.framework, basePackage)
       // Process Common files
-      this._copyJavaTemplate('common', basePackage);
+      this._copyJavaTemplate('common', basePackage)
 
     } else if (this.answers.application === 'ext-app') { // Extension app application
       // Process application.yaml.ejs file
-      this.fs.copyTpl(
-        this.templatePath('ext-app/application.yaml.ejs'),
-        this.destinationPath(path.join(BASE_RESOURCES, 'application.yaml')),
-        this.answers
-      );
+      this._copyTpl('ext-app/application.yaml', path.join(BASE_RESOURCES, 'application.yaml'));
 
       // Process scripts files
-      ['app.js.ejs', 'controller.js.ejs'].forEach(file => {
-        this.fs.copyTpl(
-          this.templatePath(path.join(COMMON_EXT_APP_TEMPLATES, 'scripts', file)),
-          this.destinationPath(path.join(BASE_RESOURCES, 'static/scripts', file.substr(0, file.indexOf('.ejs')))),
-          this.answers
-        );
-      })
+      [ 'app.js', 'controller.js' ].forEach(f => this._copyTpl(
+        path.join(COMMON_EXT_APP_TEMPLATES, 'scripts', f),
+        path.join(BASE_RESOURCES, 'static', 'scripts', f)
+      ));
 
-      this.fs.copyTpl(
-        this.templatePath('ext-app/resources/'),
-        this.destinationPath(BASE_RESOURCES),
-      );
-
-      this.fs.copyTpl(
-        this.templatePath(path.join(COMMON_EXT_APP_TEMPLATES, 'static')),
-        this.destinationPath(BASE_RESOURCES + '/static'),
-      );
-
-      this._copyJavaTemplate('ext-app/java', basePackage);
-
-      this.fs.copyTpl(
-        this.templatePath('ext-app/README.md'),
-        this.destinationPath('README.md')
-      );
+      this._copy('ext-app/resources/', BASE_RESOURCES)
+      this._copy(path.join(COMMON_EXT_APP_TEMPLATES, 'static'), BASE_RESOURCES + '/static')
+      this._copyJavaTemplate('ext-app/java', basePackage)
+      this._copy('ext-app/README.md', 'README.md')
     }
 
     // Process build files
     if (this.answers.build === 'Gradle') {
-      this._processGradleFiles();
+      this._processGradleFiles()
     } else {
-      this._processMavenFiles();
+      this._processMavenFiles()
     }
 
     // Process and copy .gitignore.tpl file
-    this.fs.copyTpl(
-      this.templatePath('.gitignore.tpl'),
-      this.destinationPath('.gitignore')
-    );
+    this._copy('.gitignore.tpl', '.gitignore')
   }
 
   /**
@@ -178,28 +140,21 @@ module.exports = class extends Generator {
    */
   install() {
     if (this.answers.build === 'Maven') {
-      try {
-        this.log('Running '.green.bold + './mvnw package'.white.bold + ' in your project'.green.bold);
-        this.spawnCommandSync(path.join(this.destinationPath(), 'mvnw'), ['package']);
-      } catch(e) {
-        try {
-          this.log('Running '.green.bold + 'mvn package'.white.bold + ' in your project'.green.bold);
-          this.spawnCommandSync('mvn', ['package']);
-        } catch(e) {
-          this.log(`${e}`.green.bold);
-        }
-      }
+      this._spawn('mvnw', 'package')
     } else {
-      try {
-        this.log('Running '.green.bold + './gradlew build'.white.bold + ' in your project'.green.bold);
-        this.spawnCommandSync(path.join(this.destinationPath(), 'gradlew'), ['build']);
-      } catch(e) {
-        try {
-          this.log('Running '.green.bold + 'gradle build'.white.bold + ' in your project'.green.bold);
-          this.spawnCommandSync('gradle', ['build']);
-        } catch(e) {
-          this.log(`${e}`.green.bold);
-        }
+      this._spawn('gradlew', 'build')
+    }
+  }
+
+  _spawn(proc, arg) {
+    try {
+      this.log('Running '.green + `${proc} ${arg}`.white + ' in your project'.green)
+      this.spawnCommandSync(path.join(this.destinationPath(), proc), [ arg ])
+    } catch(e) {
+      if (proc === 'gradle' || proc === 'mvn') {
+        this.log(`${e}`.red)
+      } else {
+        this._spawn(proc.indexOf('gradle') > -1 ? 'gradle' : 'mvn', arg)
       }
     }
   }
@@ -207,70 +162,45 @@ module.exports = class extends Generator {
   end() {
     if (this.pair) {
       this.log('\nYou can now update the service account '.cyan +
-        `${this.answers.username}`.white.bold + ` with the following public key: `.cyan);
-      this.log('\n' + this.pair.public);
-      this.log(`Please submit these details to your pod administrator.`.yellow);
-      this.log(`If you are a pod administrator, visit https://${this.answers.host}/admin-console\n`.yellow);
+        `${this.answers.username}`.white + ` with the following public key: `.cyan)
+      this.log('\n' + this.pair.public)
+      this.log(`Please submit these details to your pod administrator.`.yellow)
+      this.log(`If you are a pod administrator, visit https://${this.answers.host}/admin-console\n`.yellow)
     } else {
-      this.log('\nYou can now place the private key you received in the '.yellow + 'rsa'.white + ' directory\n'.yellow);
+      this.log('\nYou can now place the private key you received in the '.yellow + 'rsa'.white + ' directory\n'.yellow)
     }
-    this.log(`Your Java project has been successfully generated !`.cyan.bold);
+    this.log(`Your Java project has been successfully generated !`.cyan)
   }
 
   _copyJavaTemplate(dirPath, basePackage) {
-    let files = fs.readdirSync(path.join(__dirname, 'templates', dirPath))
-    files.filter(f => f.endsWith('java.ejs')).forEach(file => {
-      this.fs.copyTpl(
-        this.templatePath(path.join(dirPath, file)),
-        this.destinationPath(path.join(BASE_JAVA, basePackage, file.substr(0, file.length - 4))),
-        this.answers
-      );
-    });
+    fs.readdirSync(path.join(__dirname, 'templates', dirPath))
+      .filter(f => f.endsWith('java.ejs'))
+      .map(f => f.replaceAll(/\.ejs/g, ''))
+      .forEach(f => this._copyTpl(path.join(dirPath, f), path.join(BASE_JAVA, basePackage, f)))
   }
 
   _processGradleFiles() {
-    this.fs.copyTpl(
-      this.templatePath('gradlew'),
-      this.destinationPath('gradlew')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('gradlew.bat'),
-      this.destinationPath('gradlew.bat')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('gradle/'),
-      this.destinationPath('gradle/')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('build.gradle.ejs'),
-      this.destinationPath('build.gradle'),
-      this.answers
-    );
+    [ 'gradlew', 'gradlew.bat', 'gradle/' ].forEach(f => this._copy(f))
+    this._copyTpl('build.gradle')
   }
 
   _processMavenFiles() {
-    this.fs.copyTpl(
-      this.templatePath('mvnw'),
-      this.destinationPath('mvnw')
-    );
+    [ 'mvnw', 'mvnw.cmd', '.mvn/' ].forEach(f => this._copy(f))
+    this._copyTpl('pom.xml')
+  }
 
+  _copyTpl(fileName, destFileName) {
     this.fs.copyTpl(
-      this.templatePath('mvnw.cmd'),
-      this.destinationPath('mvnw.cmd')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('.mvn/'),
-      this.destinationPath('.mvn/')
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('pom.xml.ejs'),
-      this.destinationPath('pom.xml'),
+      this.templatePath(`${fileName}.ejs`),
+      this.destinationPath(destFileName || fileName),
       this.answers
-    );
+    )
+  }
+
+  _copy(fileName, destFileName) {
+    this.fs.copy(
+      this.templatePath(fileName),
+      this.destinationPath(destFileName || fileName)
+    )
   }
 }
--- generators/workflow-docker/templates/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Starting new container'
-docker run --rm -v ./symphony:/symphony --pull=always --name wdk-bot-app -p 8080:8080 finos/symphony-wdk:latest
